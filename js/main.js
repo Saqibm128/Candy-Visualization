d1c44cac7d177880e76ae185cae4c5032040c597
adeliyianni3 <adeliyianni5@gatech.edu>
2017-11-15 22:08:24 -0500

    started histogram

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ function candyBins(dataset, candy) {
         if (keys.indexOf(datum)>=0) {
             for (var j = 0; j<bin.length; j++) {
                 if (bin[j].key == datum) {
-                    bin[j].value.push(dataset[i]);
+                    bin[j].values.push(dataset[i]);
                 }
             }
         } else {
-            bin.push({key: datum, value: [dataset[i]]});
+            bin.push({key: datum, values: [dataset[i]]});
             keys.push(datum);
         }
     }
@@ -150,7 +150,121 @@ function(error, dataset){
 //DEMOGRAPHICS
     var goingOut = candyBins(dataset, 'Q1_GOING_OUT');
     var gender = candyBins(dataset, 'Q2_GENDER');
-    var age = candyBins(dataset, 'Q3_AGE');
+    var xScale = d3.scaleLinear()
+        .domain([0,100])
+        .range([0,3*width/4])
+    var xAxis = d3.axisBottom(xScale).ticks(8);    
+    var ageHisto = d3.histogram()
+        .domain([0,100])
+        .thresholds(xScale.ticks(80))
+        .value(function (d) { return d.Q3_AGE;})
+    const age = ageHisto(dataset).filter(d => d.length>0)
+    //var age = candyBins(dataset, 'Q3_AGE');
     var country = candyBins(dataset, 'Q4_COUNTRY');
     var provinceState = candyBins(dataset, 'Q5_STATE_PROVINCE_COUNTY_ETC');
+var candyList = [fullCandyBar, butterFinger, candyCorn, chiclets, dots, fuzzyPeaches, goodPlenty, gummyBears, healthyFruit, heathBar, darkChoco, darkMilk, hersheyKiss, jollyRancherBad, jollyRancherGood, juniorMint, kitKat, laffyTaffy, lemonHeads, licorice, licoriceBlack, lollipop, mikeIke, milkDuds, milkyWay, mMReg, mMPeanut, mintKiss, mrGoodbar, nerds, nestleCrunch, peeps, pixyStix, reecesCup, reesesPieces, rolos, skittles, snickers, sourpatch, starburst, swedishFish, ticTac, threeMusk, tolberone, trailMix, twix, whatcha, yorkPeppermint];
+// //chart 1
+//     var yScale = d3.scaleLinear()
+//         .domain([100,0])
+//         .range([0,3*height/4])
+//     var yAxis = d3.axisLeft(yScale).ticks(8);
+//     var chart = svg.append('g').attr('class', 'chart').attr('transform','translate('+width/4+','+height/8+')');
+//     chart.append('g')
+//         .attr('class', 'x-axis')
+//         .call(yAxis);
+//     var candy = chart.selectAll("dots")
+//         .data(candyList)
+//         .enter()
+//     candy.append("circle")
+//         .attr('stroke', "#0000ff")
+//         .attr("cx", function (d, i) {return width/3-8*i;})
+//         .attr("cy", function (d, i) {
+//             var joy = 0;
+//             var total = 0;
+//             for (var j=0; j<d.length; j++) {
+//                 if(d[j].key=="JOY") {
+//                     joy = d[j].values.length;
+//                 }
+//                 total = total+d[j].values.length;
+//             }
+//             return yScale(joy/total*100);
+//         })
+//         .attr("r", function (d, i) {
+//             var joy = 0;
+//             for (var j=0; j<d.length; j++) {
+//                 if(d[j].key=="JOY") {
+//                     joy = d[j].values.length;
+//                 }
+//             }
+//             return joy/50;
+//         })
+//         .on("mouseover", function() {
+//             d3.select(this).attr('r',40);
+//         })
+//         .on("mouseout", function() {
+//             d3.select(this).attr('r',function (d, i) {
+//                 var joy = 0;
+//                 for (var j=0; j<d.length; j++) {
+//                     if(d[j].key=="JOY") {
+//                         joy = d[j].values.length;
+//                     }
+//                 }
+//                 return joy/50;
+//             });
+//         });
+//chart 1
+    chartScales = {x: 'age'};
+    var yScale = d3.scaleLinear()
+        .domain([100,0])
+        .range([0,3*height/4])
+    var yAxis = d3.axisLeft(yScale).ticks(8);
+    var chart = svg.append('g').attr('class', 'chart').attr('transform','translate('+width/8+','+height/8+')');
+    chart.append('g')
+        .attr('class', 'y-axis')
+        .call(yAxis);
+    chart.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform','translate(0,'+3*height/4+')')
+        .call(xAxis);
+    chart.selectAll('ylines')
+        .data([0,10,20,30,40,50,60,70,80,90,100])
+        .enter()
+        .append('line')
+        .attr('x1', 0)
+        .attr('y1', function(d){return yScale(d)})
+        .attr('x2', 3*width/4)
+        .attr('y2', function(d){return yScale(d)})
+        .style("stroke", "#555")
+        .style("opacity",'.5')
+    for(var j=0;j<age.length;j++) {
+        var data = age[j].sort(function(a,b){
+            if (a.Q2_GENDER == b.Q2_GENDER) {
+                return 0;
+            } else if (a.Q2_GENDER == 'Male') {
+                return 1;
+            } else if (b.Q2_GENDER == 'Male') {
+                return -1;
+            } else if (a.Q2_GENDER == 'Female') {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        var people = chart.selectAll("people")
+            .data(age[j])
+            .enter()
+        people.append("circle")
+            .attr("cx", function (d, i) { return xScale(age[j].x0); })
+            .attr("cy", function (d, i) { return yScale(age[j].length-i); })
+            .attr("r", 2)
+            .attr('fill',function(d,i){
+                if(d.Q2_GENDER == "Female") {
+                    return '#ff0081';
+                } else if (d.Q2_GENDER == "Male") {
+                    return '#0d9fab';
+                } else {
+                    return '#731982';
+                }
+            });
+    }
 });
